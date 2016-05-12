15f6da218a57deb39fb4d219c13fcc168817b4d7
Mauricio <Mauricio@MauMau>
2016-05-12 15:04:33 -0300

    problema na pagina cadastros

---
+++ src/main/webapp/webjava/ImagesView.java
@@ -0,0 +1,26 @@
+package webjava;
+ 
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class ImagesView {
+     
+    private List<String> images;
+  //  private String[] nome = {"Conteudo", "Disciplima", "Pergunta", "Professor", "Prova"};
+    private String[] nome = {"Cadastro de Pergunta", "Cadastro de Professor"};
+    
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 0; i < 2; i++) {
+            images.add(nome[i]);
+        }
+    }
+ 
+    public List<String> getImages() {
+        return images;
+    }
+}
